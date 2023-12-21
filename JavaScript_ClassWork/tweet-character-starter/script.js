1b44857bd19b391a953121d996f1bd929a79e36c
Pooja Patel <128218158+PoojaPatel1712@users.noreply.github.com>
2023-12-21 18:47:33 +0530

    Delete JavaScript_ClassWork directory

--- JavaScript_ClassWork/tweet-character-starter/script.js
+++
@@ -1,7 +0,0 @@
-const textInput = document.getElementById('textInput');
-const charCounter = document.getElementById('charCounter');
-
-textInput.addEventListener('input',function(){
-    const remainingChars = 255 - textInput.value.length;
-    charCounter.textContent = remainingChars;
-})
